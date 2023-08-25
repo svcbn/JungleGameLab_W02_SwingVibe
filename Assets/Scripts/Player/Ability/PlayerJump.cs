41410861f0bb8df457f4598effbf9ecd22b2d3b1
YuTaehwan <ytw0113@gmail.com>
2023-08-25 09:43:55 +0900

    feat: Impelement Jump features

--- Assets/Scripts/Player/Ability/PlayerJump.cs
+++ Assets/Scripts/Player/Ability/PlayerJump.cs
@@ -1,24 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using W02;
 
-public class PlayerJump : MonoBehaviour
+public class PlayerJump : PlayerAbility
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float jumpHeight = 4;
 
-    // Update is called once per frame
-    void Update()
+    protected override void HandleInput()
     {
-        
-    }
+        if (_controller.collisions.below && InputManager.Instance.JumpButton) // 바닥에 있을 때만 점프 가능
+        {
+            Debug.Log(Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(_controller.gravity)));
 
-    private void FixedUpdate()
-    {
-       
+            _controller.SetYVelocity(Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(_controller.gravity)));
+        }
     }
 }
