b3a97ae97ac8370889997d701a765ad361f43a10
ByeongHan Na <qudgks7946@gmail.com>
2023-08-26 16:53:22 +0900

    add ropewarp

---
+++ Assets/Scripts/Player/Ability/PlayerRopeWarp.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRopeWarp : PlayerAbility
+{
+    
+    protected override void HandleInput()
+    {
+        Vector2 targetPosition = this.SetTargetPos();
+
+        // when jump button pressed && state == rope state
+        if (_jumpButtonClicked)
+        {
+            this.Warp(targetPosition);
+        }
+    }
+
+    /// <summary>
+    /// when warp-able object got detected, 
+    /// </summary>
+    /// <returns></returns>
+    Vector2 SetTargetPos()
+    {
+        Debug.Log(_horizontalAim + " , " + _verticalAim);
+
+        return new Vector2(-5, 5);
+    }
+    void Warp(Vector2 _targetPos)
+    {
+        this.transform.position = _targetPos;
+    }
+}
+
