b1dc25b3bb5122076010badeaf5d37c0020d04de
YuTaehwan <ytw0113@gmail.com>
2023-08-26 16:43:16 +0900

    Fix: fix strange rocket problem

--- Assets/Scripts/Player/Controller2D.cs
+++ Assets/Scripts/Player/Controller2D.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
-using UnityEngine.InputSystem.XR;
 using System;
+using UnityEngine.UIElements;
 using Unity.VisualScripting;
 
 [RequireComponent(typeof(BoxCollider2D))]
@@ -21,8 +21,11 @@ public class Controller2D : MonoBehaviour
     {
         public bool above, below;
         public bool left, right;
+        public bool wasOnGround;
 
         public Transform aboveTransform, belowTransform, leftTransform, rightTransform;
+        public GameObject[] belowObjects;
+        public GameObject standingOn;
 
         public void Reset()
         {
@@ -35,6 +38,10 @@ public class Controller2D : MonoBehaviour
     public class ControllerPhysics
     {
         [Header("Variables")]
+        [Range(0f, 0.001f)]
+        public float epsilon = 0.0001f;
+        public Vector2 maxVelocity = new Vector2(100f, 100f);
+
         [Header("중력 관련 설정")]
         public float gravity = -30f;
         [Range(0f, 2f)]
@@ -44,6 +51,7 @@ public class Controller2D : MonoBehaviour
 
         [Header("Physics value")]
         public Vector3 velocity = Vector3.zero;
+        public Vector3 externalForce = Vector3.zero;
 
         [Header("Detections")]
         public CollisionInfo collisions;
@@ -65,6 +73,10 @@ public class Controller2D : MonoBehaviour
         public float raycastHorizontalOffset = 0.05f;
         [Range(0f, 0.1f)]
         public float raycastVerticalOffset = 0.05f;
+        [Range(0f, 1f)]
+        public float raycastStickOffset = 0.2f;
+        [Range(0f, 2f)]
+        public float raycastStickLength = 1;
     }
 
     [Header("Controller Settings")]
@@ -82,12 +94,21 @@ public class Controller2D : MonoBehaviour
         }
     }
 
+    public bool WasOnGound
+    {
+        get
+        {
+            return controllerPhysics.collisions.wasOnGround;
+        }
+    }
+
     private LayerMask collisionMask;
     private float _horizontalRaySpacing;
     private float _verticalRaySpacing;
     private BoxCollider2D _collider;
     private RaycastOrigins _raycastOrigins;
-    private Vector3 _originalDeltaPos;
+    private Vector3 _deltaPos;
+    private Player _player;
 
     void Awake()
     {
@@ -95,7 +116,7 @@ public class Controller2D : MonoBehaviour
         _collider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
         collisionMask = LayerMask.GetMask("Ground");
-
+        _player = GetComponent<Player>();
     }
 
     private void Update()
@@ -115,27 +136,32 @@ public class Controller2D : MonoBehaviour
         controllerPhysics.prevCollisions = controllerPhysics.collisions;
         HorizontalCollisions(1);
         HorizontalCollisions(-1);
-        VerticalCollisions(1);
-        VerticalCollisions(-1);
+        BelowCollision();
 
-        transform.Translate(_originalDeltaPos);
+        transform.Translate(_deltaPos, Space.Self);
+        Debug.Log(_deltaPos);
+        controllerPhysics.velocity = _deltaPos / Time.deltaTime;
 
-        controllerPhysics.velocity = _originalDeltaPos / Time.deltaTime;
+        controllerPhysics.externalForce.x = 0;
+        controllerPhysics.externalForce.y = 0;
     }
 
     public void Move(Vector3 velocity)
     {
         this.controllerPhysics.velocity = velocity;
+        this.controllerPhysics.externalForce = velocity;
     }
 
     public void SetXVelocity(float xVelocity)
     {
         this.controllerPhysics.velocity.x = xVelocity;
+        this.controllerPhysics.externalForce.x = xVelocity;
     }
 
     public void SetYVelocity(float yVelocity)
     {
         this.controllerPhysics.velocity.y = yVelocity;
+        this.controllerPhysics.externalForce.y = yVelocity;
     }
 
     void ApplyGravity()
@@ -147,7 +173,8 @@ public class Controller2D : MonoBehaviour
 
     void ReadyForRaycast()
     {
-        _originalDeltaPos = controllerPhysics.velocity * Time.deltaTime;
+        _deltaPos = controllerPhysics.velocity * Time.deltaTime;
+        controllerPhysics.collisions.wasOnGround = controllerPhysics.collisions.below;
     }
 
     void HorizontalCollisions(float directionX)
@@ -156,6 +183,8 @@ public class Controller2D : MonoBehaviour
         Vector2 rayOriginBottom = (_raycastOrigins.bottomLeft + _raycastOrigins.bottomRight) / 2 + ((Vector2)transform.up * controllerSetting.raycastHorizontalError);
         Vector2 rayOriginTop = (_raycastOrigins.topLeft + _raycastOrigins.topRight) / 2 - ((Vector2)transform.up * controllerSetting.raycastHorizontalError);
 
+        bool hasCollision = false;
+        Transform targetTransform = null;
         for (int i = 0; i < controllerSetting.horizontalRayCount; i++)
         {
             Vector2 rayOrigin = Vector2.Lerp(rayOriginBottom, rayOriginTop, (float)i / (float)(controllerSetting.horizontalRayCount - 1));
@@ -167,95 +196,189 @@ public class Controller2D : MonoBehaviour
             {
                 if (Mathf.Sign(directionX) == Mathf.Sign(controllerPhysics.velocity.x))
                 {
-                    _originalDeltaPos.x = directionX * (hit.distance - _raycastOrigins.width / 2 - controllerSetting.raycastHorizontalOffset * 2);
+                    _deltaPos.x = directionX * (hit.distance - _raycastOrigins.width / 2 - controllerSetting.raycastHorizontalOffset * 2);
                 } else
                 {
-                    _originalDeltaPos.x = controllerPhysics.velocity.x * Time.deltaTime;
+                    _deltaPos.x = controllerPhysics.velocity.x * Time.deltaTime;
                 }
 
+                hasCollision = true;
+                targetTransform = hit.transform;
 
-                if (directionX == -1)
-                {
-                    controllerPhysics.collisions.left = true;
-                    controllerPhysics.collisions.leftTransform = hit.transform;
-                } else
-                {
-                    controllerPhysics.collisions.right = true;
-                    controllerPhysics.collisions.rightTransform = hit.transform;
-                }
-            } else
-            {
-                if (directionX == -1)
-                {
-                    controllerPhysics.collisions.left = false;
-                    controllerPhysics.collisions.leftTransform = null;
-                }
-                else
-                {
-                    controllerPhysics.collisions.right = false;
-                    controllerPhysics.collisions.rightTransform = null;
-                }
+                break;
             }
-
-            Platform targetPlatform = controllerPhysics.collisions.leftTransform ? controllerPhysics.collisions.leftTransform.GetComponent<Platform>() : null;
-            CallPlatformCollisionCallback(controllerPhysics.prevCollisions.left, controllerPhysics.collisions.left, targetPlatform);
-            targetPlatform = controllerPhysics.collisions.rightTransform ? controllerPhysics.collisions.rightTransform.GetComponent<Platform>() : null;
-            CallPlatformCollisionCallback(controllerPhysics.prevCollisions.right, controllerPhysics.collisions.right, targetPlatform);
         }
+
+        if (hasCollision)
+        {
+            if (directionX == -1)
+            {
+                controllerPhysics.collisions.left = true;
+                controllerPhysics.collisions.leftTransform = targetTransform;
+            }
+            else
+            {
+                controllerPhysics.collisions.right = true;
+                controllerPhysics.collisions.rightTransform = targetTransform;
+            }
+        } else
+        {
+            if (directionX == -1)
+            {
+                controllerPhysics.collisions.left = false;
+                controllerPhysics.collisions.leftTransform = null;
+            }
+            else
+            {
+                controllerPhysics.collisions.right = false;
+                controllerPhysics.collisions.rightTransform = null;
+            }
+        }
+
+        Platform targetPlatform = controllerPhysics.collisions.leftTransform ? controllerPhysics.collisions.leftTransform.GetComponent<Platform>() : null;
+        CallPlatformCollisionCallback(controllerPhysics.prevCollisions.left, controllerPhysics.collisions.left, targetPlatform);
+        targetPlatform = controllerPhysics.collisions.rightTransform ? controllerPhysics.collisions.rightTransform.GetComponent<Platform>() : null;
+        CallPlatformCollisionCallback(controllerPhysics.prevCollisions.right, controllerPhysics.collisions.right, targetPlatform);
     }
 
-    void VerticalCollisions(float directionY)
+    void BelowCollision()
     {
-        float rayLength = Mathf.Abs(_originalDeltaPos.y) + _raycastOrigins.height / 2 + controllerSetting.raycastVerticalError;
-        Vector2 rayOriginLeft = (_raycastOrigins.bottomLeft + _raycastOrigins.topLeft) / 2 + (Vector2)transform.up * controllerSetting.raycastVerticalError + (Vector2)transform.right * _originalDeltaPos.x;
-        Vector2 rayOriginRight = (_raycastOrigins.bottomRight + _raycastOrigins.topRight) / 2 + (Vector2)transform.up * controllerSetting.raycastVerticalError + (Vector2)transform.right * _originalDeltaPos.x;
+        RaycastHit2D[] rayHits = new RaycastHit2D[controllerSetting.verticalRayCount];
+        controllerPhysics.collisions.belowObjects = new GameObject[controllerSetting.verticalRayCount];
 
+        if (_deltaPos.y < -controllerPhysics.epsilon)
+        {
+            _player.playerInfo.isFalling = true;
+        } else
+        {
+            _player.playerInfo.isFalling = false;
+        }
+
+        float rayLength = _raycastOrigins.height / 2 + controllerSetting.raycastVerticalOffset;
+
+        if (_deltaPos.y < 0)
+        {
+            rayLength += Mathf.Abs(_deltaPos.y);
+        }
+
+        Vector2 rayOriginLeft = (_raycastOrigins.bottomLeft + _raycastOrigins.topLeft) / 2;
+        rayOriginLeft += (Vector2)transform.up * controllerSetting.raycastVerticalOffset;
+        rayOriginLeft += (Vector2)transform.right * _deltaPos.x;
+
+        Vector2 rayOriginRight = (_raycastOrigins.bottomRight + _raycastOrigins.topRight) / 2;
+        rayOriginRight += (Vector2)transform.up * controllerSetting.raycastVerticalOffset;
+        rayOriginRight += (Vector2)transform.right * _deltaPos.x;
+
+        float minDistance = float.MaxValue;
+        int minDistanceIdx = 0;
+        bool hasCollision = false;
         for (int i = 0; i < controllerSetting.verticalRayCount; i++)
         {
             Vector2 rayOrigin = Vector2.Lerp(rayOriginLeft, rayOriginRight, (float)i / (float)(controllerSetting.verticalRayCount - 1));
-            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
 
-            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+            rayHits[i] = RaycastWithDebug(rayOrigin, -transform.up, rayLength, collisionMask, Color.red);
 
-            if (hit)
+            if (rayHits[i])
             {
-                if (controllerPhysics.velocity.y > 0 && directionY == -1)
-                {
-                    _originalDeltaPos.y = controllerPhysics.velocity.y * Time.deltaTime;
-                } else
-                {
-                    _originalDeltaPos.y = directionY * (hit.distance - _raycastOrigins.height / 2 - controllerSetting.raycastVerticalError);
-                }
-                
-                rayLength = hit.distance;
+                hasCollision = true;
 
-                if (directionY == -1)
+                controllerPhysics.collisions.belowObjects[i] = rayHits[i].collider.gameObject;
+                if (rayHits[i].distance < minDistance)
                 {
-                    controllerPhysics.collisions.below = true;
-                    controllerPhysics.collisions.belowTransform = hit.transform;
-                } else
-                {
-                    controllerPhysics.collisions.above = true;
-                    controllerPhysics.collisions.aboveTransform = hit.transform;
-                }
-            } else
-            {
-                if (directionY == -1)
-                {
-                    controllerPhysics.collisions.below = false;
-                    controllerPhysics.collisions.belowTransform = null;
-                }
-                else
-                {
-                    controllerPhysics.collisions.above = false;
-                    controllerPhysics.collisions.aboveTransform = null;
+                    minDistance = rayHits[i].distance;
+                    minDistanceIdx = i;
                 }
             }
 
-            Platform targetPlatform = controllerPhysics.collisions.aboveTransform ? controllerPhysics.collisions.aboveTransform.GetComponent<Platform>() : null;
-            CallPlatformCollisionCallback(controllerPhysics.prevCollisions.above, controllerPhysics.collisions.above, targetPlatform);
-            targetPlatform = controllerPhysics.collisions.belowTransform ? controllerPhysics.collisions.belowTransform.GetComponent<Platform>() : null;
-            CallPlatformCollisionCallback(controllerPhysics.prevCollisions.below, controllerPhysics.collisions.below, targetPlatform);
+            if (minDistance < controllerPhysics.epsilon)
+            {
+                break;
+            }
+        }
+
+        if (hasCollision)
+        {
+            controllerPhysics.collisions.standingOn = controllerPhysics.collisions.belowObjects[minDistanceIdx];
+            _player.playerInfo.isFalling = false;
+            controllerPhysics.collisions.below = true;
+
+            if (controllerPhysics.velocity.y > 0 && controllerPhysics.externalForce.y > 0)
+            {
+                _deltaPos.y = controllerPhysics.velocity.y * Time.deltaTime;
+                controllerPhysics.collisions.below = false;
+            } else
+            {
+                _deltaPos.y = -minDistance + _raycastOrigins.height / 2 + controllerSetting.raycastVerticalOffset;
+            }
+
+            if (!controllerPhysics.collisions.wasOnGround && controllerPhysics.velocity.y > 0)
+            {
+                _deltaPos.y += controllerPhysics.velocity.y * Time.deltaTime;
+            }
+
+            if (Mathf.Abs(controllerPhysics.velocity.y) < controllerPhysics.epsilon)
+            {
+                _deltaPos.y = 0;
+            }
+        } else
+        {
+            controllerPhysics.collisions.below = false;
+        }
+
+        StickToGround();
+    }
+
+    void StickToGround()
+    {
+        if (_deltaPos.y >= controllerSetting.raycastStickOffset || 
+            _deltaPos.y <= -controllerSetting.raycastStickOffset || 
+            _player.playerInfo.isJumping ||
+            !WasOnGound || 
+            controllerPhysics.externalForce.y > 0)
+        {
+            return;
+        }
+
+        Vector2 rayOriginLeft = (_raycastOrigins.bottomLeft + _raycastOrigins.topLeft) / 2;
+        Vector2 rayOriginRight = (_raycastOrigins.bottomRight + _raycastOrigins.topRight) / 2;
+        Vector2 center = (rayOriginLeft + rayOriginRight) / 2;
+        float rayOriginY = rayOriginLeft.y;
+        rayOriginLeft.x += _deltaPos.x;
+        rayOriginRight.x += _deltaPos.x;
+
+        // TODO: 각도 있는 땅에서 걷기 시 각도 계산 필요 - 구현 추가 필요
+        /*
+        RaycastHit2D leftHit, rightHit, targetHit;
+
+        leftHit = RaycastWithDebug(rayOriginLeft, -transform.up, controllerSetting.raycastStickLength, collisionMask, Color.red);
+        rightHit = RaycastWithDebug(rayOriginRight, -transform.up, controllerSetting.raycastStickLength, collisionMask, Color.red);
+        
+        if(leftHit)
+        {
+            targetHit = leftHit;
+            targetVector = rayOriginLeft;
+        } else if (rightHit)
+        {
+            targetHit = rightHit;
+            targetVector = rayOriginRight;
+        } else
+        {
+            return;
+        }*/
+
+        RaycastHit2D stickHit = BoxCastWithDebug(
+            center,
+            new Vector2(_raycastOrigins.width, _raycastOrigins.height),
+            0,
+            -transform.up,
+            controllerSetting.raycastStickLength,
+            collisionMask,
+            Color.red);
+
+        if (stickHit)
+        {
+            _deltaPos.y = -Mathf.Abs(stickHit.point.y - rayOriginY) + _raycastOrigins.height / 2;
+            controllerPhysics.collisions.below = true;
         }
     }
 
@@ -280,10 +403,28 @@ public class Controller2D : MonoBehaviour
         }
     }
 
+    RaycastHit2D RaycastWithDebug(Vector2 origin, Vector2 direction, float distance, int layerMask, Color debugColor, bool drawDebugLine=true)
+    {
+        if (drawDebugLine)
+        {
+            Debug.DrawRay(origin, direction * distance, debugColor);
+        }
+        return Physics2D.Raycast(origin, direction, distance, layerMask);
+    }
+
+    RaycastHit2D BoxCastWithDebug(Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, int layerMask, Color debugColor, bool drawDebugLine = true)
+    {
+        RaycastHit2D boxHit = Physics2D.BoxCast(origin, size, angle, direction, distance, layerMask);
+
+        // TODO: 박스 그리기 (일단 넘김)
+
+        return boxHit;
+    }
+
     void UpdateRaycastOrigins()
     {
         Bounds bounds = _collider.bounds;
-        bounds.Expand(controllerSetting.skinWidth * -2);
+        //bounds.Expand(controllerSetting.skinWidth * -2);
 
         _raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
         _raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
